b7d21dc12925578b9df49c71d621d3d1497c1c4c
NickSamJ <samokhin1j@gmail.com>
2020-09-20 11:24:28 +0300

    .evn setup

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/firestore'
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyAteIBxonQYYU11XZazw4jmqqA9TPVcusg",
-  authDomain: "react-todo-starter.firebaseapp.com",
-  databaseURL: "https://react-todo-starter.firebaseio.com",
-  projectId: "react-todo-starter",
-  storageBucket: "react-todo-starter.appspot.com",
-  messagingSenderId: "4227661449",
-  appId: "1:4227661449:web:436214a8509f92ed199058"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
